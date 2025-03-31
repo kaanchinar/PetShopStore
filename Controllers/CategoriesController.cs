a16f19fc5536ee7dc765ff8cd66c6c2e4ff1850c
Kaan Chinar <kaanc645@gmail.com>
2025-03-31 16:47:04 +0400

    Initial commit

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pets.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cats()
+        {
+            return View();
+        }
+        public IActionResult Dogs()
+        {
+            return View();
+        }
+        public IActionResult Birds()
+        {
+            return View();
+        }
+        public IActionResult Other()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pets.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
